53d8afe2231872d18d7f1c2551474a3f1bf84c42
rajalearn90 <rajaaruna90@gmail.com>
2025-01-15 13:59:24 +0530

    Update Program.cs

--- EcommerceApplication/Program.cs
+++ EcommerceApplication/Program.cs
@@ -2,6 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddHttpsRedirection(options =>
+{
+    options.HttpsPort = null;
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
